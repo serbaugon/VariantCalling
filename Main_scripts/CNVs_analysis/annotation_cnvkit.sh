9239613485e5f759717d96834674e159fbe21dea
Sergio Baute <117904550+serbaugon@users.noreply.github.com>
2023-10-20 16:04:26 +0100

    Add files via upload

---
+++ Main_scripts/CNVs_analysis/annotation_cnvkit.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# DIRECTORIES
+DIRECTORY="/mnt/beegfs/home/serbaugon/CNVkit"
+mkdir -p $DIRECTORY/Output
+OUTPUT="$DIRECTORY/Output"
+
+# CNVs annotation
+for input in $DIRECTORY/**/*.call.cns; do
+  file="$(basename "$input")" 
+  name="$(echo $file | cut -d "." -f 1)"
+  python3 $DIRECTORY/cnv_annotate.py $DIRECTORY/refFlat.txt $input -o $OUTPUT/$name.annotated.cns
+  echo "Annotated file: $file"
+done
+
+
+
+
+
