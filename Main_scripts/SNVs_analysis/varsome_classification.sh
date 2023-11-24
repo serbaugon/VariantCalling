5a353978c44165e703f03ae739d95dbe497c064a
Sergio Baute <117904550+serbaugon@users.noreply.github.com>
2023-11-24 12:52:51 +0100

    Update varsome_classification.sh

--- Main_scripts/SNVs_analysis/varsome_classification.sh
+++ Main_scripts/SNVs_analysis/varsome_classification.sh
@@ -19,7 +19,7 @@ POST_VARSOME_SNPs_DIR="$POST_VARSOME_DIR/SNPs"
 for input in $SAMPLES_SNPs/*.vcf; do
   file="$(basename "$input")" 
   name="$(echo $file | cut -d "." -f 1)"
-  varsome_api_annotate_vcf.py -k 'dRc22Vd%IvCVgOY42IVO8!vbcSthl9t@JXOe2eJ8' -g hg38 -p add-ACMG-annotation=1 -i $input -o $POST_VARSOME_SNPs_DIR/${name}_VarSome.vcf -u 'https://api.varsome.com/'
+  varsome_api_annotate_vcf.py -k '...' -g hg38 -p add-ACMG-annotation=1 -i $input -o $POST_VARSOME_SNPs_DIR/${name}_VarSome.vcf -u 'https://api.varsome.com/'
   echo "Classified SNP file: $file"
 done
 
@@ -30,6 +30,6 @@ POST_VARSOME_INDELS_DIR="$POST_VARSOME_DIR/Indels"
 for input in $SAMPLES_INDELS/*.vcf; do
   file="$(basename "$input")" 
   name="$(echo $file | cut -d "." -f 1)"
-  varsome_api_annotate_vcf.py -k 'dRc22Vd%IvCVgOY42IVO8!vbcSthl9t@JXOe2eJ8' -g hg38 -p add-ACMG-annotation=1 -i $input -o $POST_VARSOME_INDELS_DIR/${name}_VarSome.vcf -u 'https://api.varsome.com/'
+  varsome_api_annotate_vcf.py -k '...' -g hg38 -p add-ACMG-annotation=1 -i $input -o $POST_VARSOME_INDELS_DIR/${name}_VarSome.vcf -u 'https://api.varsome.com/'
   echo "Classified INDEL file: $file"
 done
